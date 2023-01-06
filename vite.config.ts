c17c28b09499dae2ff5b797d303ca8a44b03434d
Zheheng Zhang <76390451+zhangzheheng12345@users.noreply.github.com>
2023-01-06 20:10:46 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+
+const path = require('path')
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      port: 443
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
